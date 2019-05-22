bddd862af036eba25392bc19947c9d7902b65e5c
Steve Baker <steve_m_baker@hotmail.com>
2019-05-22 16:01:41 +0100

    Changed various elements during blog writeup

--- GetsbExoPrimaryEmail/GetsbExoPrimaryEmail.psm1
+++ GetsbExoPrimaryEmail/GetsbExoPrimaryEmail.psm1
@@ -8,28 +8,27 @@ function Get-sbExoPrimaryEmail {
     BEGIN { }
 
     PROCESS {
-        # Getting all email recipients into a variable for quick access
+        # Getting all email mailboxes into a variable for quick access
         try {
-            $recipients = Get-Mailbox -Identity $Identity -ErrorAction SilentlyContinue
+            $mailboxes = Get-Mailbox -Identity $Identity -ErrorAction SilentlyContinue
         } catch {
             throw 'Please connect to Exchange Online using Connect-ExopsSession prior to running this command.'
         }
-        foreach ($recipient in $recipients) {
-            $emailaddresses = $recipient.EmailAddresses
+        foreach ($mailbox in $mailboxes) {
+            $emailaddresses = $mailbox.EmailAddresses
             foreach ($emailaddress in $emailaddresses) {
                 $smtpaddress = $emailaddress | Where-Object { $_ -clike '*SMTP*' }
             if ($smtpaddress) {
                 # Moving this outside the foreach emailaddress loop returns different results for some reason
                 [PSCustomObject]@{
-                    'Name'        = $recipient.Name
+                    'Name'        = $mailbox.Name
                     'SMTPAddress' = $smtpaddress
                 }
             } else {
                 # do nothing if no SMTP address
             } #if smtpaddress exists
         } #foreach emailaddress
-        # Write-Output $obj
-    } #foreach recipient
+    } #foreach mailbox
 } #process
 
 END { }
